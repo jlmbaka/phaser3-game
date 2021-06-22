a7865a6615d34f5630459ed2f2022336a10bb7fe
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-22 19:02:46 +0100

    load assets

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    },
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {}
+// load assets
+function preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritsheet("dude", "assets/dude.png", {
+        frameWidth: 32,
+        frameHeight: 48,
+    });
+}
 
 function create() {}
 
