9896176638403ffff67d7617bd5a2ee496e984b6
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-22 19:14:10 +0100

    Build a world

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false,
+        },
+    },
     scene: {
         preload: preload,
         create: create,
@@ -25,6 +32,14 @@ function preload() {
 
 function create() {
     this.add.image(400, 300, "sky");
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
 }
 
 function update() {}
