6839adfb0b8c28509537b6dbddb56b70f9fa872e
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-22 19:25:38 +0100

    Adds a player in the world & config its animation

--- script.js
+++ script.js
@@ -33,13 +33,38 @@ function preload() {
 function create() {
     this.add.image(400, 300, "sky");
 
-    platforms = this.physics.add.staticGroup();
+    const platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
     platforms.create(600, 400, "ground");
     platforms.create(50, 250, "ground");
     platforms.create(750, 220, "ground");
+
+    const player = this.physics.add.sprite(100, 450, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20,
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
 }
 
 function update() {}
