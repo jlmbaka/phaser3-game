f84007f9b0781b1c850224ac986f4043b1ccb43b
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-24 21:41:45 +0100

    Refactors code & add game over message

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 import constants from "../constants";
 import PauseMenu from "./PauseMenu";
 
-export default class GameScene extends Phaser.Scene {
+export default class Game extends Phaser.Scene {
     player;
     stars;
     bombs;
@@ -30,44 +30,25 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, "sky");
-
-        this.platforms = this.physics.add.staticGroup();
-
-        this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
-
-        this.platforms.create(600, 400, "ground");
-        this.platforms.create(50, 250, "ground");
-        this.platforms.create(750, 220, "ground");
-
-        this.player = this.physics.add.sprite(100, 450, "dude");
-
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        // Add stars
-        this.stars = this.physics.add.group({
-            key: "star",
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 },
-        });
-
-        this.stars.children.iterate(function (child) {
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        });
-
-        // Add bombs
-        this.bombs = this.physics.add.group();
-
-        // Display the Score
-        this.scoreText = this.add.text(16, 16, "score: 0", {
-            fontSize: "32px",
-            fill: "#000",
-        });
+        this.createPlatform();
+        this.createPlayer();
+        this.createStars();
+        this.createBombs();
+        this.createScoreText();
 
         // collide the player and the stars with the plateforms
         this.physics.add.collider(this.player, this.platforms);
         this.physics.add.collider(this.stars, this.platforms);
         this.physics.add.collider(this.bombs, this.platforms);
+
+        // check if the player has collided with any bombs
+        this.physics.add.collider(
+            this.player,
+            this.bombs,
+            this.handlePlayerBombCollide,
+            null,
+            this
+        );
 
         // check if the players overlaps with any stars
         this.physics.add.overlap(
@@ -78,24 +59,57 @@ export default class GameScene extends Phaser.Scene {
             this
         );
 
-        // check if the player has collided with any bombs
-        this.physics.add.collider(
-            this.player,
-            this.bombs,
-            this.hitBomb,
-            null,
-            this
-        );
-
         // add the menu
         this.pauseMenu = new PauseMenu(this);
     }
 
+    createPlatform() {
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
+    }
+
+    createPlayer() {
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+    }
+
+    createBombs() {
+        this.bombs = this.physics.add.group();
+    }
+
+    createStars() {
+        this.stars = this.physics.add.group({
+            key: "star",
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 },
+        });
+        this.stars.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+    }
+
+    createScoreText() {
+        // Display the Score
+        this.scoreText = this.add.text(16, 16, "score: 0", {
+            fontSize: "32px",
+            fill: "#000",
+        });
+    }
+
     update() {
         if (this.gameOver) {
+            this.displayGameOverText();
             return;
         }
+        this.updatePlayerPosition();
+        this.handlePauseMenuUpdate();
+    }
 
+    updatePlayerPosition() {
         // Controlling the player with the keyboard
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
@@ -110,7 +124,9 @@ export default class GameScene extends Phaser.Scene {
         if (this.cursors.up.isDown && this.player.body.touching.down) {
             this.player.setVelocityY(-330);
         }
+    }
 
+    handlePauseMenuUpdate() {
         if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
             // toggle modal
             if (this.pauseMenu.isOpen) {
@@ -125,34 +141,49 @@ export default class GameScene extends Phaser.Scene {
 
     collectStar(player, star) {
         star.disableBody(true, true);
-
-        // update the score
-        this.score += 10;
-        this.scoreText.setText("Score: " + this.score);
-
+        this.updateScore();
         if (this.stars.countActive(true) === 0) {
-            //  A new batch of stars to collect
-            this.stars.children.iterate(function (child) {
-                child.enableBody(true, child.x, 0, true, true);
-            });
-
-            const x =
-                player.x < 400
-                    ? Phaser.Math.Between(400, 800)
-                    : Phaser.Math.Between(0, 400);
-
-            const bomb = this.bombs.create(x, 16, "bomb");
-            bomb.setBounce(1);
-            bomb.setCollideWorldBounds(true);
-            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-            bomb.allowGravity = false;
+            this.createNewBatchOfStars(player);
         }
     }
 
-    hitBomb(player, bomb) {
+    updateScore() {
+        this.score += 10;
+        this.scoreText.setText("Score: " + this.score);
+    }
+
+    createNewBatchOfStars(player) {
+        this.stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
+
+        const x =
+            player.x < 400
+                ? Phaser.Math.Between(400, 800)
+                : Phaser.Math.Between(0, 400);
+
+        const bomb = this.bombs.create(x, 16, "bomb");
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+    }
+
+    handlePlayerBombCollide(player, bomb) {
         this.physics.pause();
         player.setTint(0xff0000);
         player.anims.play("turn");
         this.gameOver = true;
+    }
+
+    displayGameOverText() {
+        // add a You Game Over! text
+        const { width, height } = this.scale;
+        this.add
+            .text(width * 0.5, height * 0.5, "Game Over!", {
+                fontSize: 48,
+                color: "red",
+            })
+            .setOrigin(0.5);
     }
 }
